420361a7ef4bc30a4a5d61f7d387c86cdc3538f2
JnyJny <erik.oshaughnessy@gmail.com>
2025-07-15 11:00:48 -0500

    Updated tests/conftest.py
    Removed fixtures used for (removed) CLI testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,8 @@
 """ """
 
-from pathlib import Path
-import tomllib
 import pytest
 from busylight_core.hardware import ConnectionType, Hardware
 from serial.tools.list_ports_common import ListPortInfo
-
-
-@pytest.fixture(scope="session")
-def project_root() -> Path:
-    """Return the root path of the project."""
-    yield Path.cwd()
-
-
-@pytest.fixture(scope="session")
-def pyproject_path(project_root: Path) -> Path:
-    """Return the path to the pyproject.toml file."""
-    yield project_root / "pyproject.toml"
-
-
-@pytest.fixture(scope="session")
-def pyproject_toml(pyproject_path: Path) -> dict:
-    """Return the contents of the pyproject.toml file."""
-    yield tomllib.load(pyproject_path.open("rb"))
-
-
-@pytest.fixture(scope="session")
-def project_version(pyproject_toml: dict) -> str:
-    """Return the project version from pyproject.toml."""
-    return pyproject_toml["project"]["version"]
 
 
 @pytest.fixture
