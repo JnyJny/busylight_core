ba756be1fb9ff48317574b2f14f1f931c3500f58
JnyJny <erik.oshaughnessy@gmail.com>
2025-07-16 10:59:43 -0500

    EPOS Busylight Support
    Initial commit, untested.

---
+++ src/busylight_core/vendors/epos/busylight.py
@@ -0,0 +1,32 @@
+"""EPOS Busylight Support"""
+
+from functools import cached_property
+from ...light import Light
+
+from ._epos import State
+
+
+class EPOSBusylight(HIDLight):
+    supported_device_ids: dict[tuple[int, int], str] = {
+        (0x1395, 0x0074): "Busylight",
+    }
+
+    @staticmethod
+    def vendor() -> str:
+        return "EPOS"
+
+    @cached_property
+    def state(self) -> State:
+        return State()
+
+    def __bytes__(self) -> bytes:
+        return bytes(self.state)
+
+    def on(self, color: Tuple[int, int, int], led: int = 0) -> None:
+        self.color = color
+        with self.batch_update():
+            self.state.set_color(color, led)
+
+    def reset(self) -> None:
+        self.state.reset()
+        super().reset()
